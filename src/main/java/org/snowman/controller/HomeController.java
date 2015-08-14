cfdb3cc1b720b5be8224c94d9ad83823afd2aa31
xueyu <xueyua@chanjet.com>
2015-08-14 14:02:41 +0800

    spring webmvc

---
+++ src/main/java/org/snowman/controller/HomeController.java
@@ -0,0 +1,21 @@
+package org.snowman.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home.do")
+	public String home(HttpServletRequest req, HttpServletResponse resp) {
+		//return "/WEB-INF/templates/home.html";
+		System.out.println("###url is"+req.getPathInfo());
+		System.out.println("###context path:"+req.getContextPath());
+		//resp.addHeader("Content-Type", "text/html; charset=UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+		return "home";
+	}
+}
